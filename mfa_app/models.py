63028ea74baeef9ff3f375a7c72c529e1a285ae4
SudhikshaH <148217095+SudhikshaH@users.noreply.github.com>
2025-09-05 23:29:36 +0530

    Update models.py

--- mfa_app/models.py
+++ mfa_app/models.py
@@ -1,23 +1,13 @@
 from django.db import models
-<<<<<<< HEAD
-
-# Create your models here.
-=======
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import secrets
 
->>>>>>> origin/master
 class UserDetails(models.Model):
     name=models.CharField(max_length=255, unique=True)
     email=models.EmailField(max_length=255, unique=True)
     password=models.CharField(max_length=128)
-    image=models.ImageField(upload_to='captured_images/',null=True, blank=True)
-<<<<<<< HEAD
-
-    def __str__(self):
-        return self.email
-=======
+    image=models.ImageField(upload_to='captured_images/',null=True, blank=True
     token=models.CharField(max_length=64, unique=True, blank=True)
     def __str__(self):
         return self.email
@@ -27,4 +17,3 @@ def _post_save_receiver(sender, instance, created, **kwargs):
     if created and not instance.token:
         instance.token = secrets.token_hex(32)  # Generates a 64-character hexadecimal token
         instance.save()
->>>>>>> origin/master
